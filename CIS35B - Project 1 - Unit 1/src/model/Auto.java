f427c8dfb78aabe1b6e320238df885ce297ccbae
Anh <Anh@AceEnvy15>
2018-01-19 17:15:02 -0800

    worked

--- CIS35B - Project 1 - Unit 1/src/model/Auto.java
+++ CIS35B - Project 1 - Unit 1/src/model/Auto.java
@@ -9,97 +9,86 @@ import model.OptionSet.Option;
  * @author Anh
  *
  */
-public class Auto implements Serializable 
-{
+public class Auto implements Serializable {
 	private String name;
 	private int baseprice;
 	private OptionSet opset[];
+
 	/**
 	 * default constructor
 	 */
-	public Auto(String n, int p, int size)
-	{
+	public Auto(String n, int p, int size) {
 		name = n;
 		baseprice = p;
 		opset = new OptionSet[size];
 	}
-	
+
 	/**
 	 * Return name of this car model
 	 */
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * Return base price of model
 	 */
-	public int getBasePrice()
-	{
+	public int getBasePrice() {
 		return baseprice;
 	}
-	
+
 	/**
 	 * Return an OptionSet by index
 	 */
-	public OptionSet getOptionSets(int i)
-	{
+	public OptionSet getOptionSets(int i) {
 		return opset[i];
 	}
-	
-	public void setName(String n)
-	{
+
+	public void setName(String n) {
 		name = n;
 	}
-	
-	public void setBasePrice(int p)
-	{
+
+	public void setBasePrice(int p) {
 		baseprice = p;
 	}
-	
-	public int findOptionSet(String opsetName)
-	{
-		for (int i = 0; i<opset.length; i++)
-		{
-			if (opset[i].getName().equals(opsetName)) return i;
+
+	public int findOptionSet(String opsetName) {
+		for (int i = 0; i < opset.length; i++) {
+			if (opset[i].getName().equals(opsetName))
+				return i;
 		}
 		return -1;
 	}
-	
-	public boolean setOptionSet(int i, String opsetName, int opSetSize)
-	{
+
+	public boolean setOptionSet(int i, String opsetName, int opSetSize) {
 		opset[i] = new OptionSet(opsetName, opSetSize);
 		return true;
 	}
-	
-	public void displayInfo()
-	{
+
+	public void displayInfo() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("Name: ");
 		sb.append(name);
 		sb.append("\nBase Price: ");
 		sb.append(baseprice);
 		sb.append("\nOptions: ");
-		for (int i = 0; i<opset.length; i++)
-		{
-			OptionSet curOpSet = opset[i]; 
+		for (int i = 0; i < opset.length; i++) {
+			OptionSet curOpSet = opset[i];
 			sb.append("\n- ");
 			sb.append(curOpSet.getName());
 			sb.append(":");
-			for (int j = 0; j < curOpSet.getSize(); j++)
-			{
+			for (int j = 0; j < curOpSet.getSize(); j++) {
 				sb.append("\n\t");
 				sb.append("+ ");
 				sb.append(opset[i].getOption(j).getName());
 				sb.append(" : ");
 				sb.append(opset[i].getOption(j).getPrice());
-				
+
 			}
 		}
-		
+
 		System.out.println(sb.toString());
-		
+
 	}
-	
+
 }
--- CIS35B - Project 1 - Unit 1/src/util/FileIO.java
+++ CIS35B - Project 1 - Unit 1/src/util/FileIO.java
@@ -1,29 +1,26 @@
 package util;
+
 import java.io.*;
 
 import model.*;
 
 public class FileIO {
-	public Auto buildAutoObject(String filename)
-	{
-		try 
-		{
+	public Auto buildAutoObject(String filename) {
+		try {
 			BufferedReader br = new BufferedReader(new FileReader(filename));
 			String line = "";
 			line = br.readLine();
 			String name = line.split(";")[0];
 			int baseprice = Integer.parseInt(line.split(";")[1]);
 			int opSetSize = Integer.parseInt(line.split(";")[2]);
-			Auto autoObject = new Auto(name,baseprice,opSetSize);
-			
-			for (int i=0;i<opSetSize;i++)
-			{
+			Auto autoObject = new Auto(name, baseprice, opSetSize);
+
+			for (int i = 0; i < opSetSize; i++) {
 				line = br.readLine();
 				String opSetName = line.split(":")[0];
 				String options[] = line.split(":")[1].split(",");
-				autoObject.setOptionSet(i,opSetName, options.length);
-				for (int j = 0; j < options.length; j++)
-				{
+				autoObject.setOptionSet(i, opSetName, options.length);
+				for (int j = 0; j < options.length; j++) {
 					String optName = options[j].split("=")[0];
 					int optPrice = Integer.parseInt(options[j].split("=")[1]);
 					autoObject.getOptionSets(i).setOption(j, optName, optPrice);
@@ -32,11 +29,32 @@ public class FileIO {
 
 			br.close();
 			return autoObject;
-		}
-		catch (IOException e)
-		{
-			System.out.println(e.toString());
+		} catch (IOException e) {
+			System.out.println("Error: " + e.toString());
 		}
 		return null;
 	}
+
+	public void serializeAuto(Auto autoObject, String filename) {
+		try {
+			ObjectOutputStream ost = new ObjectOutputStream(new FileOutputStream(filename));
+			ost.writeObject(autoObject);
+			ost.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.toString());
+		}
+	}
+
+	public Auto deserializeAuto(String filename) {
+		try {
+			ObjectInputStream ist = new ObjectInputStream(new FileInputStream(filename));
+			Auto autoObject = (Auto) ist.readObject();
+			ist.close();
+			return autoObject;
+		} catch (Exception e) {
+			System.out.println("Error: " + e.toString());
+		}
+		return null;
+	}
+
 }
