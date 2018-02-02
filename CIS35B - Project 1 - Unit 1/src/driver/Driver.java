1a5faa08d73be5bdcd0a4317fc19c03f68db2a32
Anh Nguyen <anguyen@adobe.com>
2018-02-02 12:15:38 -0800

    almost completed: changed error numbers

--- CIS35B - Project 1 - Unit 1/src/driver/Driver.java
+++ CIS35B - Project 1 - Unit 1/src/driver/Driver.java
@@ -10,202 +10,98 @@ public class Driver {
 	 * @param args
 	 */
 	public static void main(String args[]) {
-		
-		String modelName = "ToyotaCamry";
 		CreateAuto a1 = new BuildAuto();
-		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
-//		a1.printAuto(modelName);
 		UpdateAuto a2 = new BuildAuto();
+		FixAuto a3 = new BuildAuto();
+		String modelName = "";
+
+		System.out.println("\n\n");
+		System.out.println("1. Test Working FordZTW");
+		modelName = "FordZTW";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+		a1.printAuto(modelName);
 		try {
 			a2.updateOptionSetName(modelName, "Side Impact Air Bags", "Bluetooth");
 		} catch (AutoException e) {
-			FixAuto a3 = new BuildAuto();
 			a3.fix(e.getErrorNumber());
-		}
-		finally {
+		} finally {
 			a1.printAuto(modelName);
 		}
 		try {
-			a2.updateOptionPrice(modelName, "Side Impact Air Bags", "Infra-Red Clearcoat", 1000);
+			a2.updateOptionPrice(modelName, "Color", "Infra-Red Clearcoat", 1000);
 		} catch (AutoException e) {
-			FixAuto a3 = new BuildAuto();
 			a3.fix(e.getErrorNumber());
-		}
-		finally {
+		} finally {
 			a1.printAuto(modelName);
 		}
-//		FixAuto a3 = new BuildAuto();
-//		a3.fix(102);
-//		a1.printAuto(modelName);
-		
-//		FileIO io = new FileIO();
-//		System.out.println("1. Test Working FordZTW");
-//		String modelName = "FordZTW";
-//		Auto carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("2. Test Working ToyotaCamry");
-//		modelName = "ToyotaCamry";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("3. Test Empty File");
-//		modelName = "EmptyFile";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("4. Test Missing Base Price");
-//		modelName = "MissingBasePrice";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("5. Test Missing Option");
-//		modelName = "MissingOption";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("6. Test Missing Option Name");
-//		modelName = "MissingOptionName";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("7. Test Missing Option Price");
-//		modelName = "MissingOptionPrice";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("8. Test Missing OptionSet Data");
-//		modelName = "MissingOptionSetData";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("9. Test Missing OptionSet Data 2");
-//		modelName = "MissingOptionSetData2";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
-//
-//		System.out.println("\n\n");
-//		System.out.println("10. Test Missing OptionSet Size");
-//		modelName = "MissingOptionSetSize";
-//		carModel = io.buildAutoObject("src/AutoDataFiles/" + modelName + ".txt");
-//		if (carModel != null) {
-//			System.out.println("A. Display new Auto:");
-//			System.out.println("---------------------");
-//			carModel.print();
-//			io.serializeAuto(carModel, "src/AutoDataFiles/" + modelName + ".dat");
-//			carModel = io.deserializeAuto("src/AutoDataFiles/" + modelName + ".dat");
-//			System.out.println("============================");
-//			System.out.println("B. Display deserialized Auto:");
-//			System.out.println("-----------------------------");
-//			carModel.print();
-//		} else
-//			System.out.println("Error! Input file not formatted correctly!");
+
+		System.out.println("\n\n");
+		System.out.println("2. Test Working ToyotaCamry");
+		modelName = "ToyotaCamry";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+		a1.printAuto(modelName);
+		try {
+			a2.updateOptionSetName(modelName, "Side Impact Air Bags", "Bluetooth");
+		} catch (AutoException e) {
+			a3.fix(e.getErrorNumber());
+		} finally {
+			a1.printAuto(modelName);
+		}
+		try {
+			a2.updateOptionPrice(modelName, "Color", "Infra-Red Clearcoat", 1000);
+		} catch (AutoException e) {
+			a3.fix(e.getErrorNumber());
+		} finally {
+			a1.printAuto(modelName);
+		}
+
+		System.out.println("\n\n");
+		System.out.println("3. Test Empty File");
+		modelName = "EmptyFile";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+		a1.printAuto(modelName);
+
+		System.out.println("\n\n");
+		System.out.println("4. Test Missing Base Price");
+		modelName = "MissingBasePrice";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+
+		System.out.println("\n\n");
+		System.out.println("5. Test Missing Option");
+		modelName = "MissingOption";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+
+		System.out.println("\n\n");
+		System.out.println("6. Test Missing Option Name");
+		modelName = "MissingOptionName";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+
+		System.out.println("\n\n");
+		System.out.println("7. Test Missing Option Price");
+		modelName = "MissingOptionPrice";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+
+		System.out.println("\n\n");
+		System.out.println("8. Test Missing OptionSet Data");
+		modelName = "MissingOptionSetData";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+
+		System.out.println("\n\n");
+		System.out.println("9. Test Missing OptionSet Data 2");
+		modelName = "MissingOptionSetData2";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+
+		System.out.println("\n\n");
+		System.out.println("10. Test Missing OptionSet Size");
+		modelName = "MissingOptionSetSize";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+
+		System.out.println("\n\n");
+		System.out.println("11. Test Wrong Model Info");
+		modelName = "WrongModelInfoFormat";
+		a1.buildAuto("src/AutoDataFiles/" + modelName + ".txt");
+
+		System.out.println("\n\n");
+		System.out.println("PROGRAM FINISHED!");
 	}
 }
